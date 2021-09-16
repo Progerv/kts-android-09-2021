9376008084cf9d84fb35dfe9aa2a4be6475d2a13
vasilii435 <vasilii435@yandex.ru>
2021-09-16 17:47:18 +0300

    add resource string-ru (Russian language) add new layout activity_main-land add in layout activity_main button and image

--- app/src/main/java/com/khaymoev/myapplication/MainActivity.kt
+++ app/src/main/java/com/khaymoev/myapplication/MainActivity.kt
@@ -2,10 +2,21 @@ package com.khaymoev.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var textView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.text)
+
+        findViewById<Button>(R.id.click_my).setOnClickListener {
+            textView?.text = ""
+        }
     }
 }
