ca6bdce0b8f721b27d69736d1418cea40ca4afda
vasilii435 <vasilii435@yandex.ru>
2021-10-04 18:43:24 +0300

    добавление ViewPage2  Карусель

--- app/src/main/java/com/khaymoev/myapplication/MainActivity.kt
+++ app/src/main/java/com/khaymoev/myapplication/MainActivity.kt
@@ -9,32 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Timber.d("onCreate ${hashCode()}")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Timber.d("onStart ${hashCode()}")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Timber.d("onPause ${hashCode()}")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Timber.d("onResume ${hashCode()}")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Timber.d("onStop ${hashCode()}")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Timber.d("onDestroy ${hashCode()}")
     }
 }
--- app/src/main/java/com/khaymoev/myapplication/ui/onboarding/OnBoardingFragment.kt
+++ app/src/main/java/com/khaymoev/myapplication/ui/onboarding/OnBoardingFragment.kt
@@ -3,25 +3,23 @@ package com.khaymoev.myapplication.ui.onboarding
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.khaymoev.myapplication.R
-import com.khaymoev.myapplication.databinding.FragmentOnboardingBinding
+import com.khaymoev.myapplication.databinding.FragmentOnBoardingBinding
+import com.khaymoev.myapplication.view_page_adapter.ViewPagerAdapter
+import kotlinx.android.synthetic.main.fragment_on_boarding.*
 
-class OnBoardingFragment : Fragment(R.layout.fragment_onboarding) {
+class OnBoardingFragment: Fragment(R.layout.fragment_on_boarding) {
 
-    private val binding: FragmentOnboardingBinding by viewBinding(FragmentOnboardingBinding::bind)
+    private val binding: FragmentOnBoardingBinding by viewBinding(FragmentOnBoardingBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initButtons()
+        viewPager2.adapter = ViewPagerAdapter()
 
-    }
+        val indicator = binding.indicator
+        indicator.attachToPager(viewPager2)
 
-    private fun initButtons() {
-        binding.clickMy.setOnClickListener {
-            findNavController().navigate(OnBoardingFragmentDirections.actionOnBoardingFragmentToLoginFragment())
-        }
     }
 }
